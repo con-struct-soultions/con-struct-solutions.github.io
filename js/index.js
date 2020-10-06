f8f28ddf18a5792ffe681dc4bcd95ca6f9691563
Connor Elliott <connorelliott89@gmail.com>
2020-10-05 22:31:49 -0500

    update

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ d3.select(".chart")
     .text(function(d) { return d; });
 
     // Set the date we're counting down to
-    var countDownDate = new Date("May 15, 2020 06:00:00").getTime();
+    var countDownDate = new Date("November 06, 2020 00:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
